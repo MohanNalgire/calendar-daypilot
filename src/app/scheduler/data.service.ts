3f5e805d1fc8d25ea928a68f968998c3b88880eb
Mohan Nalgire <mnalgire@gmail.com>
2019-06-18 00:32:45 +0530

    updated for data services

--- src/app/scheduler/data.service.ts
+++ src/app/scheduler/data.service.ts
@@ -3,7 +3,7 @@ import {Observable} from 'rxjs';
 import {DayPilot} from 'daypilot-pro-angular';
 import {HttpClient} from "@angular/common/http";
 import {map} from "rxjs/operators";
-
+import {UTILITIES} from '../utilities.constant';
 
 @Injectable()
 export class DataService {
@@ -12,8 +12,8 @@ export class DataService {
   }
 
   getEvents(from: DayPilot.Date, to: DayPilot.Date): Observable<any[]> {
-    console.log('from',from,'to',to);
-    return this.http.get("http://localhost:3000/events",
+    //POST — creating a new resource
+    return this.http.get(`${UTILITIES.baseURL}/events`,
     {
       params:{
         "start_gte":from.toString(),
@@ -21,22 +21,14 @@ export class DataService {
       }
     }
     ) as Observable<any>;
-    /*
-    http://localhost:3000/events?start_like=2019-06-06T18:00:00&end_like=2019-06-06T20:00:00
-    return this.http.get("/api/backend_events.php?from=" + from.toString() + "&to=" + to.toString()) as Observable<any>;
-    */
-
   }
 
   getResources(): Observable<any[]> {
-    //return this.http.get("/api/backend_resources.php") as Observable<any>;
-
-    return this.http.get("http://localhost:3000/resources") as Observable<any>;
+    return this.http.get(`${UTILITIES.baseURL}/resources`) as Observable<any>;
   }
 
   createEvent(params: CreateEventParams): Observable<CreateEventResponse> {
-    //return this.http.post("/api/backend_create.php", params).pipe(map((response:any) => {
-      return this.http.post("http://localhost:3000/events", params).pipe(map((response:any) => {
+      return this.http.post(`${UTILITIES.baseURL}/events`, params).pipe(map((response:any) => {
       return {
         id: response.id,
         start: params.start,
@@ -48,15 +40,20 @@ export class DataService {
   }
 
   moveEvent(params: MoveEventParams): Observable<any> {
-    return this.http.post("/api/backend_move.php", params);
+    //PUT — updating a resource with new and existing information
+    //but here 
+    //PATCH — modifying an existing resource with just new information
+    return this.http.patch(`${UTILITIES.baseURL}/events/${params.id}`, params);
   }
 
   deleteEvent(id: string): Observable<any> {
-    return this.http.post("/api/backend_delete.php", {id: id});
+    //
+    return this.http.delete(`${UTILITIES.baseURL}/events/${id}`);
   }
 
   updateEvent(params: UpdateEventParams): Observable<any> {
-    return this.http.post("/api/backend_update.php", params);
+    //PATCH — modifying an existing resource with just new information
+    return this.http.patch(`${UTILITIES.baseURL}/events/${params.id}`, params);
   }
 
 }
@@ -86,4 +83,5 @@ export interface MoveEventParams {
 export interface UpdateEventParams {
   id: string;
   text: string;
+  
 }
